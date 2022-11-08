0808a2d626351aeda317215f18e93cfb66f0b4c8
Priya Kushawaha <99608708+Kushpriya@users.noreply.github.com>
2022-11-08 22:04:00 +0545

    first commit

---
+++ Newton Forward interpolation.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+ float x[20], y[20][20];
+ int i,j, n;
+ printf("Enter number of data:-");
+ scanf("%d", &n);
+ printf("\nEnter data:\n");
+ for(i = 0; i < n ; i++)
+ {
+  printf("x[%d]=", i);
+  scanf("%f", &x[i]);
+  printf("y[%d]=", i);
+  scanf("%f", &y[i][0]);
+ }
+ for(i = 1; i < n; i++)
+ {
+  for(j = 0; j < n-i; j++)
+  {
+   y[j][i] = y[j+1][i-1] - y[j][i-1];
+  }
+ }
+ printf("\nFORWARD DIFFERENCE TABLE\n\n");
+ for(i = 0; i < n; i++)
+ {
+  printf("%0.2f", x[i]);
+  for(j = 0; j < n-i ; j++)
+  {
+   printf("\t%0.2f", y[i][j]);
+  }
+  printf("\n");
+ }
+ getch(); 
+ return 0;
+}
